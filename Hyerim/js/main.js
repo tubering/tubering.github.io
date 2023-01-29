68a03cffe3a545aec3a65c20e0c60c7c880ca025
tubering <123735735+tubering@users.noreply.github.com>
2023-01-29 14:27:15 +0900

    Add files via upload

---
+++ Hyerim/js/main.js
@@ -0,0 +1,11 @@
+$( document ).ready( function() {
+    $( '.up' ).click( function() {
+      $( 'html, body' ).scrollTop( 50 );
+    });
+
+    $( '.down' ).click( function() {
+        ($( 'html, body' ).scrollTop(2613.60009765625 ));
+      });
+
+
+  } );
